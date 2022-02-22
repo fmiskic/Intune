f5eefe656723f12d7566c58cd23791e70b18cce2
fmiskic <48210659+fmiskic@users.noreply.github.com>
2022-02-22 23:26:59 +0100

    Delete RunAddPrinter.cmd

--- Scripts/Printers/RunAddPrinter.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%AddPrinter.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'"
